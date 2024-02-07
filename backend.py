41a8bbfd0a6526a4ece3e31d4c6e9ef18bcdb199
Fabio <fabio.manzoni@gmail.com>
2024-02-07 09:24:55 +0100

    fixed. Was vscode bug in closing the terminal window

--- backend.py
+++ backend.py
@@ -1,26 +1,40 @@
-import os
 from flask import Flask, send_file, jsonify
-
+# use .env to load environment variables
+import os
+import dotenv
 app = Flask(__name__)
 
+# Load environment variables from .env file
+dotenv.load_dotenv()
 folder_path = os.environ.get('mp3_folder')
 
-@app.route('/api/mp3/<filename>', methods=['GET'])
-def get_mp3(filename):
-    # Assuming your mp3 files are stored in the network folder "\\mini-toaster\music\The Ventures - Oldies & Goldies"
-    file_path = folder_path + '{}'.format(filename)
+# New route for the root endpoint
+@app.route('/', methods=['GET'])
+def hello_world():
+    return 'Hello, World!'
 
+@app.route('/api/mp3/<int:file_id>', methods=['GET'])
+def get_mp3(file_id):
     try:
-        return send_file(file_path, mimetype='audio/mpeg')
-    except FileNotFoundError:
-        return 'File not found', 404
+        files = [f for f in os.listdir(folder_path) if f.endswith('.mp3')]
+        files.sort()
 
-@app.route('/api', methods=['GET'])
-def list_mp3_files():
-    files = os.listdir(folder_path)
-    files_json = jsonify(files)
-    print(files_json)
-    return 1
+        if 0 <= file_id < len(files):
+            filename = files[file_id]
+            file_path = os.path.join(folder_path, filename)
+
+            custom_message = "Hello, you accessed file_id {} which corresponds to {}".format(file_id, filename)
+
+            print(custom_message)
+            #play the file
+            return send_file(file_path, as_attachment=True)
+
+        else:
+            return 'Invalid file_id', 404
+    except Exception as e:
+        return 'Error: {}'.format(str(e)), 500
 
 if __name__ == '__main__':
-    app.run()
+    #run in port 5001, debug mode
+    app.run(port=5001, debug=True)
+
